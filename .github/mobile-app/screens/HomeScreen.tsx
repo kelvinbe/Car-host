04bc01114838e0bf1b51344046c0d74b7ec8e4eb
Donald Onyino <58218526+porkytheblack@users.noreply.github.com>
2022-11-16 15:57:51 +0300

    feat: created expo mobile app with initial config, redux and navigation

---
+++ .github/mobile-app/screens/HomeScreen.tsx
@@ -0,0 +1,28 @@
+import { StyleSheet } from 'react-native'
+import React from 'react'
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
+import { RootStackParamList } from '../types'
+import { Text, View } from '../components/Themed'
+
+const HomeScreen = ({navigation, route}: NativeStackScreenProps<RootStackParamList, 'HomeScreen'>) => {
+  return (
+    <View style={styles.container} >
+      <Text style={styles.title} >Divvly Home Screen</Text>
+    </View>
+  )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100%",
+        width: "100%",
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: "bold"
+    }
+})
---
+++ .github/mobile-app/store/slices/index.ts
@@ -0,0 +1,9 @@
+import {combineReducers} from  "@reduxjs/toolkit"
+import counterSlice from "./counterSlice";
+
+
+const rootReducer = combineReducers({
+    counter: counterSlice
+});
+
+export default rootReducer
